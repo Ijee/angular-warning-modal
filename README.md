03825c7b05e98bf5d143fa225b07a06754b9c5b7
ijee <15277745+Ijee@users.noreply.github.com>
2024-06-18 19:54:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Angular Warning Modal
+
+Just for archiving purposes. It's not even good code or looks any good. Also only works with Bulma 1.0.0 or whatever.
---
+++ warning-modal/warning-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { WarningDialogService } from './warning-dialog.service.ts';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+@Component({
+    selector: 'app-warning-modal',
+    templateUrl: './warning-modal.component.html',
+    styleUrls: ['./warning-modal.component.scss'],
+    standalone: true,
+    imports: [FaIconComponent]
+})
+export class WarningModalComponent {
+    constructor(public readonly warningDialog: WarningDialogService) {}
+}
